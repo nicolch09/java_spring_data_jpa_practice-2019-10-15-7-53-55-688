799e9a4274a0cef3db0c622c3dc52726984bbc62
CHRISTOPER NICOLAS (DOCU-IRIS-ISD-OOCLL/MNL) <NICOLCH@OOCL.COM>
2019-10-16 15:16:32 +0800

    [Nicolas & Ken Lising] Added One to Many relationship of Company to Employee

--- src/main/java/com/tw/apistackbase/core/Company.java
+++ src/main/java/com/tw/apistackbase/core/Company.java
@@ -1,6 +1,7 @@
 package com.tw.apistackbase.core;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 public class Company {
@@ -10,6 +11,9 @@ public class Company {
 
     @OneToOne(cascade = CascadeType.ALL)
     private CompanyProfile companyProfile;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Employee> employees;
 
     private String name;
 
@@ -35,6 +39,14 @@ public class Company {
 
     public void setCompanyProfile(CompanyProfile companyProfile) {
         this.companyProfile = companyProfile;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
     }
 
     public Company() {
