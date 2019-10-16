55d666ed764967cf87fb1bac815c00ad98c69103
CHRISTOPER NICOLAS (DOCU-IRIS-ISD-OOCLL/MNL) <NICOLCH@OOCL.COM>
2019-10-16 17:19:23 +0800

    [Nicolas & Ken Lising] Responses returned fixed

--- src/main/java/com/tw/apistackbase/controller/CompanyController.java
+++ src/main/java/com/tw/apistackbase/controller/CompanyController.java
@@ -3,7 +3,12 @@ package com.tw.apistackbase.controller;
 import com.tw.apistackbase.core.Company;
 import com.tw.apistackbase.repository.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/companies")
@@ -13,11 +18,36 @@ public class CompanyController {
     CompanyRepository companyRepository;
 
     @GetMapping(produces = {"application/json"})
+    @ResponseStatus(code = HttpStatus.OK)
     public Iterable<Company> list() {
         return companyRepository.findAll();
     }
 
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<List<Company>> delete(@PathVariable Long id){
+        Optional<Company> foundCompany = companyRepository.findById(id);
+        if (foundCompany.isPresent()) {
+            companyRepository.deleteById(id);
+            return new ResponseEntity<>(companyRepository.findAll(), HttpStatus.OK);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PatchMapping(path = "/{id}", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<Company> modify(@RequestBody Company company, @PathVariable Long id){
+        Optional<Company> foundCompany = companyRepository.findById(id);
+
+        if (foundCompany.isPresent()) {
+            Company modifyCompany = foundCompany.get();
+            modifyCompany.setName(company.getName());
+            Company savedCompany = companyRepository.save(modifyCompany);
+            return new ResponseEntity<>(savedCompany, HttpStatus.OK);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
     @PostMapping(produces = {"application/json"})
+    @ResponseStatus(code = HttpStatus.CREATED)
     public Company add(@RequestBody Company company) {
         return companyRepository.save(company);
     }
